916db90b7124725081e616c00d2bcd60e6a6ed7d
VoidLeech <ian.gerritse@gmail.com>
2024-08-28 21:20:17 +0200

    api: advancement helper does the work of serverplayer checking

--- src/main/java/com/github/voidleech/oblivion/advancement/AdvancementHelper.java
+++ src/main/java/com/github/voidleech/oblivion/advancement/AdvancementHelper.java
@@ -4,9 +4,14 @@ import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementProgress;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 
 public class AdvancementHelper {
-    public static void grantByName(String name, ServerPlayer serverPlayer) {
+
+    public static void grantByName(String name, Player player) {
+        if (!(player instanceof ServerPlayer serverPlayer)){
+            return;
+        }
         Advancement advancement = serverPlayer.server.getAdvancements().getAdvancement(new ResourceLocation(name));
         AdvancementProgress progress = serverPlayer.getAdvancements().getOrStartProgress(advancement);
         while (progress.getRemainingCriteria().iterator().hasNext()){
